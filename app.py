725f97dd77a182478a33194d9102c4ec82dea55c
Tiago Henrique <tiago_henrique@hotmail.com>
2026-04-24 13:25:49 -0300

    Change database loading from CSV to Excel

--- app.py
+++ app.py
@@ -7,7 +7,11 @@ import plotly.express as px
 st.set_page_config(layout='wide')
 st.title("Dados HBOnco")
 
-database = pd.read_csv("dados/indicadores.csv")
+try:
+    database = pd.read_excel(st.secrets['DATABASE'])
+except Exception as e:
+    st.error(f"Erro ao carregar o banco de dados: {e}")
+    st.stop()
 
 database = database.rename(columns={
     'sitio_primario___1':'Mama', 'sitio_primario___2':'Pulmão', 'sitio_primario___3':'C&P', 'sitio_primario___4':'SNC', 'sitio_primario___5':'Ovário', 'sitio_primario___6':'Próstata', 'sitio_primario___7':'Sarcoma', 'sitio_primario___8':'Esôfago', 'sitio_primario___9':'Via Biliar', 'sitio_primario___10':'Pênis', 'sitio_primario___11':'Gástrico', 'sitio_primario___12':'Pâncreas', 'sitio_primario___13':'Colorretal', 'sitio_primario___14':'Colo Útero', 'sitio_primario___15':'Endométrio', 'sitio_primario___16':'Fígado', 'sitio_primario___17':'Pele', 'sitio_primario___18':'Bexiga', 'sitio_primario___19':'Rim', 'sitio_primario___20':'Outro', 'sitio_primario___21':'Sarcomas', 'outro_sitio_primario':'Outro sítio primário', 'estagio_clinico':'Estágio clínico', 'metastase___1':'M Fígado', 'metastase___2':'M Pulmão', 'metastase___3':'M SNC', 'metastase___4':'M Peritônio', 'metastase___5':'M Osso', 'metastase___6':'M Linfonodos', 'metastase___7':'M Adrenal', 'metastase___8':'M Outro', 'metastase___9':'Não se aplica','metastase___10':'M Pleura', 'metastase___11':'Progressão locoregional - em cenário paliativo'})
